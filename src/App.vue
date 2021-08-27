f25979ee288631fffa4b2c74f907ef277d474826
Nikita Shamin <nikich201198@gmail.com>
2021-08-27 19:06:56 +0300

    feat/style: make main button

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <Button @click.native="showPopup"/>
+  </div>
+</template>
+
+<script>
+import Button from './components/Button.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Button
+  },
+  data() {
+    return {
+      popupOpen: false
+    }
+  },
+  methods: {
+    showPopup() {
+      this.popupOpen = true
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+  @font-face {
+    font-family: 'Lab Grotesque';
+    src: url("./fonts/LabGrotesque-Regular.ttf") format("truetype");
+    font-weight: normal;
+ }
+
+*{
+  box-sizing: border-box;
+}
+html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+body {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background: linear-gradient(255.35deg, #DC3131 0.83%, rgba(255, 79, 79, 0) 108.93%), #FF5E56;
+  background-repeat: no-repeat;
+  box-shadow: 0px -0.11px 16.9495px rgba(183, 187, 225, 0.33);
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  font-family: 'Lab Grotesque';
+  font-style: normal;
+  font-weight: 500;
+  margin: 0;
+}
+#app{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,58 @@
+<template>
+  <button class="button">
+    Налоговый вычет
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../assets/mixins.scss';
+
+.button{
+  @include display-flex-cntr;
+  font-family: 'Lab Grotesque';
+  font-style: normal;
+  font-weight: 500;
+  top: 50vh;
+  left: 50vw;
+  flex-direction: row;
+  border: 1px solid #FFFFFF;
+  filter: drop-shadow(0px 0px 44px #CFDAE7);
+  border-radius: 6px;
+  background: transparent;
+  color: #FFFFFF;
+  white-space: nowrap;
+  transition: .2s ease-in-out;
+  &:hover{
+      background: #FFFFFF;
+      color: #000000;
+    }
+}
+
+@media screen and (min-width: 0px) {
+  .button{
+    padding: 12px 24px;
+    width: 149px;
+    height: 40px;
+    font-size: 12px;
+    line-height: 16px;
+    cursor: pointer;
+  }
+}
+@media screen and (min-width : 768px) {
+  .button{
+    padding: 16px 32px;
+    width: 198px;
+    height: 56px;
+    font-size: 16px;
+    line-height: 24px;
+  }
+}
+</style>
